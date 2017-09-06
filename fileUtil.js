94265bfa78bb0d96d43d0ff2e3b7e0ed82d84d82
xw <616525957@qq.com>
2017-09-07 00:52:51 +0800

    add vs launch config and fileUtil function

--- fileUtil.js
+++ fileUtil.js
@@ -7,19 +7,19 @@
     //输出引用项
     var fileUtil = {};
 
-    var filePaten = []; //指定的文件类型
+    var filePatten = []; //指定的文件类型
     var filePath = process.cwd(); //默认路径
 
-    //获取路径下所有满足要求的文件 如：path="log/" paten=['js','txt']
+    //获取路径下所有满足要求的文件 如：path="log/" patten=['js','txt']
     //返回指定目录下所有满足条件的文件（绝对路径，名称）
-    fileUtil.getAllFilesByPath = function(path, paten) {
+    fileUtil.getAllFilesByPath = function(path, patten) {
         path = path || filePath;
-        paten = paten || filePaten;
+        patten = patten || filePatten;
         fileList = [];
-        return getAllFiles(path, paten);
+        return getAllFiles(path, patten);
     };
 
-    function getAllFiles(path, paten) {
+    function getAllFiles(path, patten) {
         var res = [],
             files = fs.readdirSync(path);
         files.forEach(function(file, index) {
@@ -27,12 +27,27 @@
                 stat = fs.lstatSync(pathname);
 
             if (!stat.isDirectory()) {
-                res.push({ "path": pathname, "filename": file });
+                //过滤指定文件类型
+                if (patten && patten.length > 0 && patten.indexOf(fileUtil.getFileSuffix(file)) < 0) {;
+                } else {
+                    res.push({ "path": pathname, "filename": file });
+                }
+
             } else {
-                res = res.concat(getAllFiles(pathname));
+                res = res.concat(getAllFiles(pathname, patten));
             }
         });
         return res;
+    }
+
+    fileUtil.getFileSuffix = function(filename) {
+        if (!filename) return '';
+        var index = filename.lastIndexOf(".");
+        if (index > -1 && filename != ".") {
+            return filename.substr(index + 1);
+        } else {
+            return "";
+        }
     }
 
     //删除目录及其下所有文件
